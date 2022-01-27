cb3c104d7c9e3535ef9f507b941c487b23b8b2f2
YuryangKim <flowratekim@gmail.com>
2022-01-27 21:48:06 +0900

    Add : toggle files

---
+++ src/utils/toggle/Toggle.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Toggle = styled.div`
+  width: 100%;
+  margin: 20px;
+  border: 2px solid #ddd;
+  font-size: 20px;
+`;
---
+++ src/utils/toggle/toggle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Toggle.style';
+
+function Toggle() {
+  return (
+    <S.Toggle>
+
+    </S.Toggle>
+  );
+}
+
+export default Toggle;
