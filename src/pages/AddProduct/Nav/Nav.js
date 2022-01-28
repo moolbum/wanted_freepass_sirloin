87b4ab4c9101a48ff15abe40a2eab4a5bb24c98d
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-01-29 00:47:07 +0900

    Add : 상품저장하기 버튼 추가

--- src/pages/AddProduct/Nav/Nav.js
+++ src/pages/AddProduct/Nav/Nav.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import ProductOptionsButton from '../ProductOptions/ProductOptionsButton/ProductOptionsButton';
 import * as S from './Nav.style';
 
 const Nav = () => {
+  const submit = () => {
+    return alert('저장');
+  };
+
   return (
     <S.NavContainer>
       <S.NavWrap>
         <S.Nav>상품 등록</S.Nav>
-        <S.Nav>상품 등록</S.Nav>
+        <ProductOptionsButton
+          children="저장하기"
+          bgColor={props => props.theme.blue}
+          borderColor={props => props.theme.blue}
+          color={props => props.theme.white}
+          style={{ marginRight: '50px' }}
+          onClick={submit}
+        />
       </S.NavWrap>
     </S.NavContainer>
   );
